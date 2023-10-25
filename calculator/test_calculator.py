a99f5573023596887b4b60f72fbc2e2b9e1b3fdb
Jyoti Lal <lalakku12@gmail.com>
2023-10-25 06:19:44 +0100

    added calculator functionality and test cases

---
+++ calculator/test_calculator.py
@@ -0,0 +1,23 @@
+import calculator
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_addition2(self):
+        assert 5 == calculator.add(2, 3)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_multplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
+
+
+
+
+
+
+
+        
