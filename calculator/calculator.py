5d9a6e09e8c103f31cd05f7d97e27eddda59b3ba
Jyoti Lal <lalakku12@gmail.com>
2023-10-27 01:24:38 +0100

    modify config.yml

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,9 +1,15 @@
 
+
 def add(first_num, second_num):
+    print("two lines between the functions")
     return first_num + second_num
 
+
 def multiply(first_num, second_num):
+    print("two lines between the functions")
     return first_num * second_num
 
+
 def subtract(first_num, second_num):
+    print("two lines between the functions")
     return first_num - second_num
